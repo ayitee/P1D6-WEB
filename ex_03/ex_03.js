2b77bc3c2aa615053595d669dce34d6e229451cb
ayitee <matis.gaudin@epitech.digital>
2024-10-07 23:08:08 +0200

    Update ex_03.js

--- ex_03/ex_03.js
+++ ex_03/ex_03.js
@@ -1,23 +1,18 @@
-function addCarToAgent(agent, brand, model, color, power) {
+function addCarToAgent(Agent, brand, model, color, power) {
   if (!brand || !model || !color || !power) {
-    console.error('Missing or incorrect car information');
+    console.error('ERREUR MON POTE');
     return null;
   }
-  agent.car = {
+  Agent.car = {
     brand,
     model,
     color,
     power
   };
-  return agent;
-}
-
-/*
-  - Vérifie si toutes les informations de la voiture sont fournies.
-  - Affiche une erreur si des informations sont manquantes.
-  - Retourne null si des informations sont incorrectes ou manquantes.
-  - Ajoute les informations de la voiture à l'objet agent.
-  - Marque de la voiture (brand), modèle (model), couleur (color), et puissance (power).
-  - Retourne l'objet agent mis à jour avec les informations de la voiture.
-*/
-
+  return Agent;
+} 
+// Checks if all of the required car information is provided.
+// Logs an error message and returns null if any information is missing.
+// Creates a car object with the provided brand, model, color, and power.
+// Assigns the car object to the agent's car property.
+// Returns the updated agent object.
