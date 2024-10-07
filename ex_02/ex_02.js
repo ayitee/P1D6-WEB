cf98d83f302bdff52d886a5ee64b42aa6c61204a
ayitee <matis.gaudin@epitech.digital>
2024-10-07 23:06:33 +0200

    Update ex_02.js

--- ex_02/ex_02.js
+++ ex_02/ex_02.js
@@ -2,11 +2,7 @@ function addCar(agentString, car) {
   const agent = JSON.parse(agentString);
   agent.car = car;
   return agent;
-}
-
-/*
-  - Convertit la chaîne JSON `agentString` en un objet JavaScript.
-  - Ajoute l'objet `car` en tant que propriété `car` de l'agent.
-  - Retourne l'objet agent mis à jour avec les informations de la voiture.
-*/
-
+} 
+// Parses the agentString JSON into an object.
+// Assigns the provided car to the agent object.
+// Returns the updated agent object with the new car property.
